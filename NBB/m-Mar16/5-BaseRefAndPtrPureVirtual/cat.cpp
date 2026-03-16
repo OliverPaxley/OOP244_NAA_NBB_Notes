5b2241046734906a45c2a5fd35b489dafa0380d0
Fardad Soleimanloo <fardad.soleimanloo@senecapolytechnic.ca>
2026-03-16 13:19:10 -0400

    Mar16 NBB

--- NBB/m-Mar16/5-BaseRefAndPtrPureVirtual/cat.cpp
+++ NBB/m-Mar16/5-BaseRefAndPtrPureVirtual/cat.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
-using namespace std;
 #include "Utils.h"
 #include "Cat.h"
+using namespace std;
+
 namespace seneca {
-   Cat::Cat(const char* thename, int numOfLives)
-//       :Animal{ thename }, m_numOfLives{ numOfLives } {
-     :Animal(thename), m_numOfLives(numOfLives){
-           if (seneca::debug) cout << "as a cat with " << m_numOfLives << " lives" << endl;
+   Cat::Cat(const char* thename, int numOfLives):Animal(thename), m_numOfLives(numOfLives){
+ //     m_numOfLives = numOfLives;
+     if(seneca::debug) cout << "Creating " << name() << " the Cat with " << m_numOfLives << " lives"   << endl;
    }
    void Cat::act() {
       cout << "Act playful, " << name() << " the Cat" << endl;
@@ -15,11 +15,7 @@ namespace seneca {
       cout << "Move like " << name() << " the Cat" << endl;
    }
    void Cat::sound() {
-      Animal::sound();
       cout << "Meow!" << endl;
-   }
-   void Cat::play() {
-      cout << name() << " the cat is playing!" << endl;
    }
    Cat::~Cat() {
       if (seneca::debug) cout << "Removing " << name() << " the Cat" << endl;
